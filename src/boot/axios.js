46124bc86ba0c8123d08b239e1088dc4e6a5c901
FinixEz <102864074+FinixEz@users.noreply.github.com>
2023-11-28 10:52:58 +0700

    Update axios.js

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const RESTAPI = "http://localhost:3000/api"
+const RESTAPI = "https://web-programming-back-end-pk9w.vercel.app/api"
 // const RESTAPI = "https://manage-account-api.vercel.app/api"
 const api = axios.create({ 
   baseURL: RESTAPI,
